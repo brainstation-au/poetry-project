f86bdc4259bee303ccd68585cee5705fb30f409e
Khalid Saifullah <khalid@outlook.com.au>
2023-04-10 10:04:26 +1000

    fix assets copy

--- new-project.sh
+++ new-project.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu
+
 curl -sSL https://install.python-poetry.org | python3 -
 export PATH="/root/.local/bin:$PATH"
 
@@ -9,10 +11,10 @@ poetry new $PROJECT_NAME
 
 cd $PROJECT_NAME
 python -m venv ./.venv
-poetry add pytest pytest-cov pylint pytest-picked pytest-sugar pytest-xdist --group test
 export PATH="$(pwd)/.venv/bin:$PATH"
+poetry add pytest pytest-cov pylint pytest-picked pytest-sugar pytest-xdist --group test
 
-cp -r ../assets/ .
+cp -r ../assets/. .
 
 pylint --generate-rcfile > ./.pylintrc
-echo "[CUSTOM]\n\ndisable=missing-module-docstring,missing-function-docstring\n" >> ./.pylintrc
+echo -e "\n[CUSTOM]\n\ndisable=missing-module-docstring,missing-function-docstring" >> ./.pylintrc
